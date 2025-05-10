2668b5c679bdb57149b00ac829f4e381182ec5f0
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-10 12:22:10 -0500

    feat: actualizando navegacion para incluir vistas de login y registro

--- app/src/main/java/com/midam/guardian/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/midam/guardian/presentation/navigation/AppNavigation.kt
@@ -1,25 +1,46 @@
 package com.midam.guardian.presentation.navigation
 
-import androidx.compose.runtime.*
-import androidx.navigation.*
-import androidx.navigation.NavHost
-import androidx.navigation.compose.*
-import com.midam.guardian.presentation.screen.main.*
-import com.midam.guardian.presentation.screen.splash.*
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.midam.guardian.presentation.screen.main.MainScreen
+import com.midam.guardian.presentation.screen.splash.SplashScreen
+import com.midam.guardian.presentation.screen.login.LoginScreen
+import com.midam.guardian.presentation.screen.register.RegisterScreen
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = SplashScreen.route){
+    NavHost(navController = navController, startDestination = SplashScreen.route) {
         composable(SplashScreen.route) {
             SplashScreen {
-                navController.navigate(MainScreen.route) {
+                navController.navigate(LoginScreen.route) {
                     popUpTo(SplashScreen.route) { inclusive = true }
                 }
             }
         }
-        composable(MainScreen.route){
+        composable(MainScreen.route) {
             MainScreen()
+        }
+        composable(LoginScreen.route) {
+            LoginScreen(
+                onLoginSuccess = {
+                    navController.navigate(MainScreen.route) {
+                        popUpTo(LoginScreen.route) { inclusive = true }
+                    }
+                },
+                onNavigateToRegister = {
+                    navController.navigate(RegisterScreen.route)
+                }
+            )
+        }
+        composable(RegisterScreen.route) {
+            RegisterScreen {
+                navController.navigate(LoginScreen.route) {
+                    popUpTo(RegisterScreen.route) { inclusive = true }
+                }
+            }
         }
     }
 }
