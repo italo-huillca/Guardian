6a47418051bddd73ba42d7eb214209a6bc5a8367
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-10 12:21:41 -0500

    feat: añadiendo objetos de las vistas en AppScreens.kt

--- app/src/main/java/com/midam/guardian/presentation/navigation/AppScreens.kt
+++ app/src/main/java/com/midam/guardian/presentation/navigation/AppScreens.kt
@@ -3,11 +3,21 @@ package com.midam.guardian.presentation.navigation
 import kotlinx.serialization.Serializable
 
 @Serializable
-object SplashScreen{
+object SplashScreen {
     const val route = "Splash"
 }
 
 @Serializable
-object MainScreen{
+object MainScreen {
     const val route = "Main"
+}
+
+@Serializable
+object LoginScreen {
+    const val route = "Login"
+}
+
+@Serializable
+object RegisterScreen {
+    const val route = "Register"
 }
