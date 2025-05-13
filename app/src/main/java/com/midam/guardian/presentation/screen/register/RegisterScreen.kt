8f6766cbd0adbb661736278b72a913a7ca0ef3e3
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-13 11:54:34 -0500

    feat: agregar logo en la pantalla de registro

--- app/src/main/java/com/midam/guardian/presentation/screen/register/RegisterScreen.kt
+++ app/src/main/java/com/midam/guardian/presentation/screen/register/RegisterScreen.kt
@@ -1,19 +1,24 @@
 package com.midam.guardian.presentation.screen.register
 
 import android.widget.Toast
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.midam.guardian.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,10 +50,20 @@ fun RegisterScreen(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
-                    .padding(16.dp),
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "logo guardian",
+                    contentScale = ContentScale.FillHeight,
+                    modifier = Modifier.size(300.dp)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
                 OutlinedTextField(
                     value = email,
                     onValueChange = { email = it },
