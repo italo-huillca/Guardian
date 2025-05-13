b338e390b85464642515e0fd1b7697e04fc38c10
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-13 11:53:57 -0500

    feat: agregar imagen de logo en la pantalla de inicio de sesión

--- app/src/main/java/com/midam/guardian/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/midam/guardian/presentation/screen/login/LoginScreen.kt
@@ -1,16 +1,21 @@
 package com.midam.guardian.presentation.screen.login
 
 import android.widget.Toast
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.*
+import androidx.compose.ui.modifier.*
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.*
 import com.google.firebase.auth.FirebaseAuth
+import com.midam.guardian.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,6 +49,7 @@ fun LoginScreen(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                Image(painter = painterResource(id = R.drawable.logo), contentDescription = "logo guardian", contentScale = ContentScale.FillHeight,modifier = Modifier.size(300.dp))
                 OutlinedTextField(
                     value = email,
                     onValueChange = { email = it },
