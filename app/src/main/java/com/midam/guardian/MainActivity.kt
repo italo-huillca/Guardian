2a9b200d7d66371463d6ec49c8c96cf33f83aa0a
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-13 13:38:29 -0500

    fix: removiendo edge-to-edge de MainActivity

--- app/src/main/java/com/midam/guardian/MainActivity.kt
+++ app/src/main/java/com/midam/guardian/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this)
-        enableEdgeToEdge()
         setContent {
             GuardianTheme {
                 AppNavigation()
